8e79aaf189d07771fbcfd41e1c9ebfd66d716ea5
Ayush tomar <ayushtomar388@gmail.com>
2024-10-13 22:23:27 +0530

    link added

--- src/components/ToolCard.tsx
+++ src/components/ToolCard.tsx
@@ -10,15 +10,15 @@ const ToolCard = ({
     href: string;
 }) => {
     return ( 
-        <div className="border border-[#195CC8] flex flex-col gap-4 justify-center items-center text-center p-14 px-16 w-fit rounded-2xl bg-[#E8F0FC]">
-            <Image
-                src={imgsrc}
-                alt={title}
-                width={80}
-                height={80}
-            />
-            <h3>{title}</h3>
-        </div>
+        <a href={href} className="border border-[#195CC8] flex flex-col gap-4 justify-center items-center text-center p-14 px-16 w-fit rounded-2xl bg-[#E8F0FC]">
+                <Image
+                    src={imgsrc}
+                    alt={title}
+                    width={80}
+                    height={80}
+                />
+                <h3>{title}</h3>
+        </a>
      );
 }
  
