9aaaba86cb39a557a482af959dfd9d9c2feee27f
Ayush tomar <ayushtomar388@gmail.com>
2024-10-17 22:33:07 +0530

    Styled

--- src/components/ToolCard.tsx
+++ src/components/ToolCard.tsx
@@ -9,16 +9,26 @@ const ToolCard = ({
     imgsrc: string;
     href: string;
 }) => {
-    return ( 
-        <a href={href} className="border border-[#195CC8] flex flex-col gap-4 justify-center items-center text-center p-14 px-16 w-fit rounded-2xl bg-[#E8F0FC]">
+    return (
+        <a className="w-36 min-h-36 p-5 bg-white flex flex-col gap-4 rounded-lg shadow-lg">
                 <Image
+                    className="m-auto"
                     src={imgsrc}
                     alt={title}
-                    width={80}
-                    height={80}
+                    width="80"
+                    height="80"
                 />
-                <h3>{title}</h3>
-        </a>
+                <p className="text-center">{title}</p>
+        </a> 
+        // <a href={href} className="border border-[#195CC8] flex flex-col gap-4 justify-center items-center text-center p-14 px-16 w-fit rounded-2xl bg-[#E8F0FC]">
+        //         <Image
+        //             src={imgsrc}
+        //             alt={title}
+        //             width={80}
+        //             height={80}
+        //         />
+        //         <h3>{title}</h3>
+        // </a>
      );
 }
  
