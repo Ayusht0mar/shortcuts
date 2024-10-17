c08d04410120b2207ddd3089ceb2ecc84c694ac7
Ayush tomar <ayushtomar388@gmail.com>
2024-10-18 00:40:27 +0530

    Styled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="bg-[#F5F5F7] h-full inset-0 -z-10 w-full overflow-x-hidden bg-[radial-gradient(#C4C4C6_1px,transparent_1px)] [background-size:16px_16px]">
+        <div className="bg-[#F5F5F7] min-h-screen inset-0 -z-10 w-full overflow-x-hidden bg-[radial-gradient(#C4C4C6_1px,transparent_1px)] [background-size:16px_16px]">
           {children}
         </div>
         <Analytics/>
