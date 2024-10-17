b095022f7fee8d051226c5b384d29c41d514714a
Ayush tomar <ayushtomar388@gmail.com>
2024-10-18 00:06:00 +0530

    href added

--- src/components/ToolCard.tsx
+++ src/components/ToolCard.tsx
@@ -10,15 +10,15 @@ const ToolCard = ({
     href: string;
 }) => {
     return (
-        <a className="w-36 min-h-36 p-5 bg-white flex flex-col gap-4 rounded-lg shadow-lg">
-                <Image
-                    className="m-auto"
-                    src={imgsrc}
-                    alt={title}
-                    width="80"
-                    height="80"
-                />
-                <p className="text-center">{title}</p>
+        <a className="w-36 h-36 py-5 bg-white flex flex-col space-y-3 rounded-lg shadow-lg" href={href}>
+                  <Image
+                        className="m-auto"
+                        src={imgsrc}
+                        alt={title}
+                        width="80"
+                        height="80"
+                    />
+                <p className="text-center text-sm text-[#94a3b8]">{title}</p>
         </a> 
         // <a href={href} className="border border-[#195CC8] flex flex-col gap-4 justify-center items-center text-center p-14 px-16 w-fit rounded-2xl bg-[#E8F0FC]">
         //         <Image
