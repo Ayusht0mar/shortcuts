65ba828ff60112ea405502e1e38e4ef7ed06aacc
Ayush tomar <ayushtomar388@gmail.com>
2024-10-17 22:32:53 +0530

    Img removed

--- src/components/DevelopmentShortcuts.tsx
+++ src/components/DevelopmentShortcuts.tsx
@@ -47,18 +47,10 @@ const tools = [
 const DevelopmentShortcuts = () => {
     return ( 
         <div className="w-[84vw] mx-auto md:flex justify-between gap-8">
-            <div className="flex flex-col gap-4">
-                <Image
-                    src="/developmenticon.svg"
-                    alt="Development Icon"
-                    height={60}
-                    width={60}
-                />
-                <h2 className="font-extrabold text-2xl">
-                    Development <br /> shortcuts
-                </h2>
-            </div>
-            <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 justify-start w-fit">
+            <h2 className="font-extrabold text-2xl">
+                Development <br /> shortcuts
+            </h2>
+            <div className="grid grid-cols-2 lg:grid-cols-5 gap-6 justify-start w-fit">
                 {tools.map((tools) => (
                     <ToolCard key={tools.title} {...tools}/>
                 ))}
