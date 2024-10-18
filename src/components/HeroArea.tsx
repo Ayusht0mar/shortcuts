ef646dbce0da6c115d0a0dffff42cda8f4ebd2e5
Ayush tomar <ayushtomar388@gmail.com>
2024-10-18 22:44:24 +0530

    Styled

--- src/components/HeroArea.tsx
+++ src/components/HeroArea.tsx
@@ -1,10 +1,10 @@
 const HeroArea = () => {
     return ( 
-        <div className="w-[84vw] mx-auto my-20">
-            <h1 className="font-extrabold text-5xl leading-snug text-balance max-w-2xl">All the shortcuts you need, organized and searchable in one place</h1>
+        <div className="w-[84vw] mx-auto my-36">
+            <h1 className="font-extrabold text-5xl leading-snug text-balance max-w-2xl text-[#86868C]">All the shortcuts you need, organized and searchable in one place</h1>
             <div className="mt-5">
-                <p className="text-sm">Created By</p>
-                <a href="https://www.ayushtomar.in/" className="font-semibold">Ayush Tomar</a>
+                <p className="text-sm text-[#86868C]">Created By</p>
+                <a href="https://www.ayushtomar.in/" className="font-semibold text-[#86868C]">Ayush Tomar</a>
             </div>
         </div>
      );
