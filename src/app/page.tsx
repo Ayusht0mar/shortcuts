e6cf732569e2a8f0633d24092dea93586456719a
Ayush tomar <ayushtomar388@gmail.com>
2024-10-19 21:31:02 +0530

    Sponsor section commented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import DevelopmentShortcuts from "@/components/DevelopmentShortcuts";
 import Header from "@/components/Header";
 import HeroArea from "@/components/HeroArea";
 import ProductivityShortcuts from "@/components/ProductivityShortcuts";
-import SponsorCard from "@/components/SponsorCard";
+// import SponsorCard from "@/components/SponsorCard";
 
 export default function Home() {
   return (
