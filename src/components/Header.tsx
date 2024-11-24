c21e4c247d8dc3225614cc81d9093715ab60599d
Ayush tomar <ayushtomar388@gmail.com>
2024-11-24 23:39:53 +0530

    Bg changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 
 const Header = () => {
     return ( 
-        <header className="py-3 border-b-2 border-[#195CC8]">
-          <div className="w-[84vw] mx-auto">
-                  <Image
-                    src="/logo.svg"
-                    alt="Logo"
-                    width={48}
-                    height={48}
-                    />
-            
+        <header className="py-6 px-6">
+          <div className="bg-white rounded-full py-4 px-8">
+            <div className="flex justify-between ">
+                    <Image
+                      src="/logo.svg"
+                      alt="Logo"
+                      width={48}
+                      height={48}
+                      />
+                      <button>Sponsor</button>
+            </div>
           </div>
         </header>
      );
