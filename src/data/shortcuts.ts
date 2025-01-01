480dd575d08a733a8386f9ef61ba8fdb02fd7cef
Ayush tomar <ayushtomar388@gmail.com>
2025-01-01 22:55:12 +0530

    Dummy data added

---
+++ src/data/shortcuts.ts
@@ -0,0 +1,48 @@
+export const shortcuts = [
+    {
+        tool : "Notion",
+        shortcuts : [
+            {
+                category: "General",
+                content : [
+                    {
+                        action: "Copy",
+                        windows: "Ctrl  + C",
+                        mac: "Cmd + C"
+                    },
+                    {
+                        action: "Paste",
+                        windows: "Ctrl  + V",
+                        mac: "Cmd + V"
+                    },
+                    {
+                        action: "Cut",
+                        windows: "Ctrl  + X",
+                        mac: "Cmd + X"
+                    }
+                ]
+            },
+            {
+                category: "Text Formatting",
+                content : [
+                    {
+                        action: "Bold",
+                        windows: "Ctrl  + B",
+                        mac: "Cmd + B"
+                    },
+                    {
+                        action: "Italic",
+                        windows: "Ctrl  + I",
+                        mac: "Cmd + I"
+                    },
+                    {
+                        action: "Underline",
+                        windows: "Ctrl  + U",
+                        mac: "Cmd + U"
+                    }
+                ]
+            }
+
+        ]
+    },
+]
