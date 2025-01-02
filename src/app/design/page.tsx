21d5d3d6e4a6a6726993f326377716ede2bdcd4c
Ayush tomar <ayushtomar388@gmail.com>
2025-01-02 23:34:04 +0530

    Created design page

---
+++ src/app/design/page.tsx
@@ -0,0 +1,15 @@
+import DesignShortcuts from "@/components/DesignShortcuts";
+import Header from "@/components/Header";
+
+const DesignPage = () => {
+    return ( 
+        <div>
+            <div className="bg-[#3f75ff]">
+                <Header/>
+            </div>
+            <DesignShortcuts/>
+        </div>
+     );
+}
+ 
+export default DesignPage;
