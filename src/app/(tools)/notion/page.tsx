0240a9bc61c404d472ce9f00ea4185d5997e9508
Ayush tomar <ayushtomar388@gmail.com>
2025-01-02 23:34:58 +0530

    Styled

--- src/app/(tools)/notion/page.tsx
+++ src/app/(tools)/notion/page.tsx
@@ -2,7 +2,7 @@ import ShortcutsTable from "@/components/ShortCutsTable";
 
 const NotionShortcutsPage = () => {
     return ( 
-        <div className="w-[90vw] mx-auto my-16 max-w-7xl">
+        <div className="w-[96vw] md:w-[90vw] mx-auto my-16 max-w-7xl">
             <h1 className="text-5xl text-[#B7B7BF] font-extrabold">Notion Shortcuts</h1>
             <p className="text-[#B7B7BF] text-lg mt-4 max-w-xl">Notion is an all-in-one workspace for your notes, tasks, wikis, and databases. It is a great tool for organizing your life and work. Here are some shortcuts to help you navigate Notion faster.</p>
             <ShortcutsTable pageTool="Notion"/>
