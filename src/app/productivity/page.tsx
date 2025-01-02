81751c2eab810d08c3cae92e370eabcccf25ac9d
Ayush tomar <ayushtomar388@gmail.com>
2025-01-02 23:33:11 +0530

    Created productivity page

---
+++ src/app/productivity/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import ProductivityShortcuts from "@/components/ProductivityShortcuts";
+
+const ProductivityPage = () => {
+    return ( 
+        <div>
+            <div className="bg-[#3f75ff]">
+                <Header/>
+            </div>
+            <ProductivityShortcuts/>
+        </div>
+     );
+}
+ 
+export default ProductivityPage;
