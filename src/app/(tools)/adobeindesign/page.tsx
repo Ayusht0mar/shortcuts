73c8a96e3d4f4ce5c12228f285bac53d6aa9a257
Ayush tomar <ayushtomar388@gmail.com>
2025-01-02 23:32:22 +0530

    Crated Design tools pages

--- src/app/(tools)/adobeindesign/page.tsx
+++ src/app/(tools)/adobeindesign/page.tsx
@@ -1,17 +1,15 @@
 import ShortcutsTable from '@/components/ShortCutsTable';
 
-const AdobeAfterEffects = () => {
+const AdobeAfterIndesign = () => {
 
 
     return ( 
         <div className="w-[96vw] md:w-[90vw] mx-auto my-16 max-w-7xl">
-        <h1 className="text-5xl text-[#B7B7BF] font-extrabold">Adobe After Effects Shortcuts</h1>
+        <h1 className="text-5xl text-[#B7B7BF] font-extrabold">Adobe Indesign Shortcuts</h1>
         <p className="text-[#B7B7BF] text-lg mt-4 max-w-xl">Adobe After Effects is a  .</p>
-        <ShortcutsTable pageTool="Adobe After Effects"/>
+        <ShortcutsTable pageTool="Adobe Indesign"/>
     </div>
      );
 }
  
-export default AdobeAfterEffects;
-
-
+export default AdobeAfterIndesign;
--- src/app/(tools)/adobephotoshop/page.tsx
+++ src/app/(tools)/adobephotoshop/page.tsx
@@ -1,17 +1,14 @@
 import ShortcutsTable from '@/components/ShortCutsTable';
 
-const AdobeAfterEffects = () => {
-
+const AdobePhotoshopPage = () => {
 
     return ( 
         <div className="w-[96vw] md:w-[90vw] mx-auto my-16 max-w-7xl">
-        <h1 className="text-5xl text-[#B7B7BF] font-extrabold">Adobe After Effects Shortcuts</h1>
+        <h1 className="text-5xl text-[#B7B7BF] font-extrabold">Adobe Photoshop Shortcuts</h1>
         <p className="text-[#B7B7BF] text-lg mt-4 max-w-xl">Adobe After Effects is a  .</p>
-        <ShortcutsTable pageTool="Adobe After Effects"/>
+        <ShortcutsTable pageTool="Adobe Photoshop"/>
     </div>
      );
 }
  
-export default AdobeAfterEffects;
-
-
+export default AdobePhotoshopPage;
