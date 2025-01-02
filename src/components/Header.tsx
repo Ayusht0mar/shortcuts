b99f05f10c77bf085ffffa91e7032c69656d0722
Ayush tomar <ayushtomar388@gmail.com>
2025-01-02 23:28:10 +0530

    Styled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,19 +5,25 @@ const Header = () => {
     return ( 
         <header className="p-4 md:p-6">
           <div className="bg-white rounded-full py-2 md:py-4 px-4 md:px-8">
-            <div className="flex justify-between ">
-                    <Image
-                      src="/logo.svg"
-                      alt="Logo"
-                      width={48}
-                      height={48}
-                      />
-                      <ul className="flex items-center gap-5 text-[#758493]">
+            <div className="flex justify-between items-center">
+                    <a href="/">
+                      <Image
+                        src="/logo.svg"
+                        alt="Logo"
+                        width={48}
+                        height={48}
+                        />
+                    </a>
+                      <ul className="hidden lg:flex items-center gap-5 text-[#758493]">
                         <Link href="/development">Development</Link>
                         <Link href="/design">Design</Link>
                         <Link href="/productivity">Productivity</Link>
                       </ul>
-                      <button className="bg-[#3f75ff] p-2 px-4 h-fit my-auto rounded-full text-white"><a href="/sponsor">Sponsor</a></button>
+                        <a href="/sponsor">
+                          <button className="bg-[#3f75ff] p-2 px-4 h-fit my-auto rounded-full text-white">
+                            Sponsor
+                          </button>
+                        </a>
             </div>
           </div>
         </header>
