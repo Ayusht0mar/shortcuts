217b5ead86b314067e5139cdcb988c642a09c715
Ayush tomar <ayushtomar388@gmail.com>
2025-01-02 23:39:27 +0530

    Created productivity tools pages

---
+++ src/app/(tools)/gmail/page.tsx
@@ -0,0 +1,13 @@
+import ShortcutsTable from "@/components/ShortCutsTable";
+
+const GmailShortcutsPage = () => {
+    return ( 
+        <div className="w-[96vw] md:w-[90vw] mx-auto my-16 max-w-7xl">
+            <h1 className="text-5xl text-[#B7B7BF] font-extrabold">Gmail Shortcuts</h1>
+            <p className="text-[#B7B7BF] text-lg mt-4 max-w-xl">Notion is an all-in-one workspace for your notes, tasks, wikis, and databases. It is a great tool for organizing your life and work. Here are some shortcuts to help you navigate Notion faster.</p>
+            <ShortcutsTable pageTool="Gmail"/>
+        </div>
+     );
+}
+ 
+export default GmailShortcutsPage;
